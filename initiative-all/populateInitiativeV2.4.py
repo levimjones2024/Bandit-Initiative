c854c952e672733b51e80deb46bab3d62dff40bb
BuildTools <unconfigured@null.spigotmc.org>
2024-04-02 15:23:47 -0500

    Added attack and damage rollers to initiative tracker

--- initiative-all/populateInitiativeV2.4.py
+++ initiative-all/populateInitiativeV2.4.py
@@ -1,10 +1,16 @@
 import random
 
-#new line to make them different
-#now more differenter
+#if you wish to use the health and damage aids alongside the initiative tracker, add them here with the format:
+#"Name": [Health, attack-modifier, number-of-dice-rolled, size-of-dice, damage-modifier ]
+
+NPCUnit = {
+    "Bandit": [30, +3, 2, 8, 3],
+    "null": [0, 0, 0, 0, 0,]
+}
 
 def main():
     summary = []
+
 
     while (True):
         action = input("What do you want to do now?\nType 0 for help\n")
@@ -39,8 +45,31 @@ def runInitiative(summary, prettyList):
 
         for i in prettyList:
             placeCounter += 1
+            attackRoll = ""
+            damageRoll = 0
+            unitUse = "null"
+            d20Roll = 0
+
+            for key in NPCUnit:
+                if key in i:
+                    unitUse = key
+
+
             if placeCounter == initiativeCounter:
-                print(i, "<---")
+                if "^" in i:
+                    d20Roll = random.randint(1, 20)
+                    for j in range(NPCUnit[unitUse][2]):
+                        damageRoll += random.randint(1, NPCUnit[unitUse][3])
+                    damageRoll += NPCUnit[unitUse][4]
+                    if d20Roll == 20:
+                        damageRoll += (NPCUnit[unitUse][2]*NPCUnit[unitUse][3]+NPCUnit[unitUse][4])
+                        print(i, "<---"  + f" Attack Roll: CRITICAL HIT; Damage roll: {damageRoll}")
+                    else:
+                        attackRoll = d20Roll + NPCUnit[unitUse][1]
+                        print(i, "<---"  + f" Attack Roll: {attackRoll}; Damage roll: {damageRoll}")
+                else: 
+                    print(i, "<---")
+
             else:
                 print(i)
 
@@ -58,6 +87,8 @@ def makeList():
     unitName = input("What is their name? ")
     modifier = int(input("What is their modifier? "))
     unitNumber = int(input("How many of them are there? "))
+    global NPCUnit
+
     #iterates unitNumber times to populate the list
     for i in range(unitNumber):
         d20 = random.randint(1, 20)
@@ -66,7 +97,10 @@ def makeList():
         if initiative < 10:
             initiative = "0" + str(initiative)
 
-        unit = str(initiative) + " " + unitName.rstrip() + str(i+1)
+        if unitName in NPCUnit.keys():
+            unit = str(initiative) + " " + unitName.rstrip() + str(i+1) + "^"
+        else:
+            unit = str(initiative) + " " + unitName.rstrip() + str(i+1)
         initiativeList.append(unit)
 
     return initiativeList
